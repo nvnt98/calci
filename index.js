582e7e5c881f53cb5ab6b6ada79673053d7eb4d6
Navneet Gond <146729662+nvnt98@users.noreply.github.com>
2023-11-15 04:43:30 +0530

    index.js

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 let currentDisplay = '' ;
 document.querySelector('#input_display').value = currentDisplay;
+function randomColor() {
+      var letters = "0123456789ABCDEF";
+      var color = "#";
+      for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+      }
+      return color;
+    }
+
+    document.documentElement.style.setProperty('--start-color', randomColor());
+    document.documentElement.style.setProperty('--middle-color', randomColor());
+    document.documentElement.style.setProperty('--end-color', randomColor());
